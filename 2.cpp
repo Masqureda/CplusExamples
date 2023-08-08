7111ebcbb08a92a027e9c4b8bdf60b38da6e9ed9
Kadir Köroğlu <122994783+Masqureda@users.noreply.github.com>
2023-08-08 23:10:12 +0300

    2.cpp

---
+++ 2.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	string isim = "Ali cabbar";
+	char not_ortalamasi = 'A';
+	int yas = 20;
+	float sayisal_ort = 3.25;
+	bool sinavaGirdimi = true; 
+	double pi = 3.14159;
+
+	cout << "Ogrenci adi-soyadi :" << isim << endl;
+	cout << "Not ortalamasi :" << not_ortalamasi << endl;
+	cout << "Yasi :" << yas << endl;
+	cout << "Ortalama :" << sayisal_ort << endl;
+	cout << "Sinav yoklamasi :" << sinavaGirdimi << endl;
+	cout << "pi sayisi :" << pi << endl;
+	return 0;
+}
