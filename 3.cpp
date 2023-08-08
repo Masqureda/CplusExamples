1c0afb0c92abb560f55de11e0b33655ea4445ab3
Kadir Köroğlu <122994783+Masqureda@users.noreply.github.com>
2023-08-08 23:40:40 +0300

    Dikdörtgen alan hesaplayıcı

---
+++ 3.cpp
@@ -0,0 +1,13 @@
+int main() {
+	cout << "Merhaba" << endl;
+	
+	int en, boy, alan;
+	cout << "Lutfen diktortgenin enini giriniz :" << endl;
+	cin >> en;
+	cout << "Lutfen diktortgenin boyunu giriniz :" << endl;
+	cin >> boy;
+	alan = en * boy;
+	cout << "Dikdörtgenin alani :" << alan << endl;
+	
+	
+}
